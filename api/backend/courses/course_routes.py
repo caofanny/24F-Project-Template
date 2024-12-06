9ef7a0704e401c3eff83e7cd9a97fc29e352bb3d
caofanny <cao.fa@northeastern.edu>
2024-12-06 14:36:35 -0500

    deleted unused databases and fixed connection errors to database

--- api/backend/courses/course_routes.py
+++ api/backend/courses/course_routes.py
@@ -1,7 +1,3 @@
-########################################################
-# Sample customers blueprint of endpoints
-# Remove this file if you are not using it in your project
-########################################################
 import datetime
 from flask import Blueprint
 from flask import request
@@ -9,7 +5,6 @@ from flask import jsonify
 from flask import make_response
 from flask import current_app
 from backend.db_connection import db
-from backend.ml_models.model01 import predict
 
 #------------------------------------------------------------
 # Create a new Blueprint object, which is a collection of 
