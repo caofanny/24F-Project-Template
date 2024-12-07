9cfb1d0f5d4db07836a18baa96c7b71a69787a02
emilyt16 <tsao.ti@northeastern.edu>
2024-12-07 06:26:12 -0500

    Revert "adding reports functionality"
    This reverts commit b1405fed83910989688f415d1b63b2a798fe9ee2.

--- app/src/pages/Make_Report.py
+++ app/src/pages/Make_Report.py
@@ -17,42 +17,4 @@ SideBarLinks()
 # set the header of the page
 st.header('Make a Report')
 
-st.write("We value our community guidelines greatly and if you feel like they were violated, please make a report:")
-
-BASE_URL = "http://api:4000/r/reports"  # Adjust the URL as needed
-
-#form that will get the input from the user
-with st.form("report_form"):
-        user_reported = st.text_input("User Reported (UserID):", "")
-        report_reason = st.text_area("Reason for the Report:")
-        submit_button = st.form_submit_button("Submit Report")
-
-
-#uploading the new user into the database
-if submit_button:
-    if user_reported and report_reason:
-        try:
-            # Prepare the data payload
-            user_data = {
-                "UserReported": user_reported,
-                "Reason": report_reason,
-            }
-            # Send POST request
-            response = requests.post(BASE_URL, json=user_data)
-            if response.status_code == 200:
-                st.success("Report successfully submitted")
-            else:
-                st.error(f"Failed to create report: {response.text}")
-        except Exception as e:
-            st.error(f"An error occurred: {str(e)}")
-    else:
-        st.error("Please fill in all fields.")
-    
-
-
-    
-        
-
-
-
-
+st.write('Here are a couple of graphs and tables that visualize how Co-op search is going for your students!')
