f86b8efc9db46af11f0544fb3ed3ef5ce27a2990
haderie <haderie49@gmail.com>
2024-12-07 09:39:43 -0500

    changed button order

--- app/src/pages/3_Mentor_Home.py
+++ app/src/pages/3_Mentor_Home.py
@@ -14,15 +14,15 @@ st.write('')
 st.write('')
 st.write('### What would you like to do today?')
 
+if st.button('View Your Profile and Connections', 
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/Mentor_Experience_Page.py')
+
 if st.button('Your Posts', 
              type='primary',
              use_container_width=True):
   st.switch_page('pages/Posts_Page.py')
-
-if st.button('View Your Experiences', 
-             type='primary',
-             use_container_width=True):
-  st.switch_page('pages/Mentor_Experience_Page.py')
 
 if st.button("Connect with Students/View Students",
              type='primary',
